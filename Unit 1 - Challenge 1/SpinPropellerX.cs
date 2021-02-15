360c45811bb13fcad66c394865611d2daa632cf1
William Pike <arcaroth@gmail.com>
2021-02-14 21:43:08 -0600

    debugging and adding
    debugged a couple of lines to allow plane to move forward correctly. along with the camera able to follow the plane in Unity
    also followed the bonus challenge to add rotation to the propeller on the plane

---
+++ Unit 1 - Challenge 1/SpinPropellerX.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpinPropellerX : MonoBehaviour
+{
+    // rotation speed of propeller
+    public float rpm;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        // rotating propeller by the rpm 
+        transform.Rotate(Vector3.forward * rpm);
+    }
+}
