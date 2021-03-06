2812efe5988ad5a779971dfd2361fc49030df9a2
William Pike <arcaroth@gmail.com>
2021-03-06 12:05:15 -0600

    Create PlayerController.cs
    added horizontal input for the player in this module

---
+++ Unit 2 - Prototype 2/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    public float horizontalInput;
+    public float speed = 10.0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        horizontalInput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+    }
+}
