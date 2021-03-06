9cbbb2e246e9abe47e4bda100010a6ba8eaf7b58
William Pike <arcaroth@gmail.com>
2021-03-06 12:21:28 -0600

    Update PlayerController.cs
    updated to add boundries to the x value so the player does not move out of the screen.

--- Unit 2 - Prototype 2/PlayerController.cs
+++ Unit 2 - Prototype 2/PlayerController.cs
@@ -6,6 +6,7 @@ public class PlayerController : MonoBehaviour
 {
     public float horizontalInput;
     public float speed = 10.0f;
+    public float xRange = 15.0f;
 
     // Start is called before the first frame update
     void Start()
@@ -16,6 +17,15 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        if (transform.position.x < -xRange)
+        {
+            transform.position = new Vector3(-xRange, transform.position.y, transform.position.z);
+        }
+
+        if (transform.position.x < xRange)
+        {
+            transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
+        }
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
     }
