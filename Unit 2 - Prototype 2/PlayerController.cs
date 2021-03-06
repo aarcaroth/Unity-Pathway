d06fd1c920d43b6531429077372e2222d8d9ee24
William Pike <arcaroth@gmail.com>
2021-03-06 13:13:24 -0600

    Update PlayerController.cs
    updated so that when we press space bar, a projectile will launch from the player

--- Unit 2 - Prototype 2/PlayerController.cs
+++ Unit 2 - Prototype 2/PlayerController.cs
@@ -8,6 +8,8 @@ public class PlayerController : MonoBehaviour
     public float speed = 10.0f;
     public float xRange = 15.0f;
 
+    public GameObject projectilePrefab;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -26,7 +28,13 @@ public class PlayerController : MonoBehaviour
         {
             transform.position = new Vector3(xRange, transform.position.y, transform.position.z);
         }
+
         horizontalInput = Input.GetAxis("Horizontal");
         transform.Translate(Vector3.right * Time.deltaTime * horizontalInput * speed);
+
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(projectilePrefab, transform.position, projectilePrefab.transform.rotation);
+        }
     }
 }
