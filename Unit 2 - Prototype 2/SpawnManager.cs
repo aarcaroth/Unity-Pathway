4c5e3988d3bd3cdce525ea3d6790687fbcf8bc47
William Pike <arcaroth@gmail.com>
2021-03-07 12:03:58 -0600

    Update SpawnManager.cs
    cleaned things up so we can use private variables instead of numbers

--- Unit 2 - Prototype 2/SpawnManager.cs
+++ Unit 2 - Prototype 2/SpawnManager.cs
@@ -6,6 +6,9 @@ public class SpawnManager : MonoBehaviour
 {
     public GameObject[] animalPrefabs;
 
+    private float spawnRangeX = 15;
+    private float spawnPosZ = 15;
+
     // Start is called before the first frame update
     void Start()
     {
@@ -17,8 +20,12 @@ public class SpawnManager : MonoBehaviour
     {
         if (Input.GetKeyDown(KeyCode.S))
         {
+            // randomly generate animal and spawn position
+            Vector3 spawnPos = new Vector3(Random.Range(-spawnRangeX, spawnRangeX), 0, spawnPosZ);
+
             int animalIndex = Random.Range(0, animalPrefabs.Length);
-            Instantiate(animalPrefabs[animalIndex], new Vector3(Random.Range(-15, 15), 0, 20), animalPrefabs[animalIndex].transform.rotation);
+
+            Instantiate(animalPrefabs[animalIndex], spawnPos, animalPrefabs[animalIndex].transform.rotation);
         }    
     }
 }
